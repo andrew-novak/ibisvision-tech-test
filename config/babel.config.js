da8acba0182cb190c35da2537acd3e7d634dda2f
Andrew Novak <contact@andrewnovak.co.uk>
2021-10-28 04:28:30 +0100

    api.cache(true) in babel config

--- config/babel.config.js
+++ config/babel.config.js
@@ -2,6 +2,8 @@ module.exports = api => {
   const presets = ["@babel/preset-env", "@babel/preset-react"];
   const plugins = ["@babel/transform-runtime"];
 
+  api.cache(true);
+
   return {
     presets,
     plugins
